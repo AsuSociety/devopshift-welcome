4ac4f28e8cc91522794bed7330061685adbace3f
Your Name <you@example.com>
2025-02-04 13:43:08 +0000

    lab102 solution

--- labs/lab102/main.tf
+++ labs/lab102/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     time = {
       source = "hashicorp/time"
-      version = "0.7.2"  # Ensure you're using the latest version
+      version = "0.7.2"  
     }
   }
 }
@@ -44,13 +44,13 @@ resource "aws_instance" "vm" {
 }
 
 resource "time_sleep" "wait_for_ip" {
-  create_duration = "10s"  # Wait for 30 seconds to allow Azure to allocate the IP
+  create_duration = "10s"  
 }
 
 
 output "vm_public_ip" {
   value       = aws_instance.vm.public_ip
-  depends_on  = [time_sleep.wait_for_ip]  # Wait for the time_sleep resource to complete
+  depends_on  = [time_sleep.wait_for_ip]  
   description = "Public IP address of the VM"
 }
 
